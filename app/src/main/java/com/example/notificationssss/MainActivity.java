69d5323f3bbcf3235ed1a7e7ffd276f6396fe6fd
wuwujusz <161115124+wuwujusz@users.noreply.github.com>
2025-02-04 09:06:10 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/notificationssss/MainActivity.java
+++ app/src/main/java/com/example/notificationssss/MainActivity.java
@@ -1,15 +1,17 @@
-package com.example.notificationssss;
-
+package com.example.not;
+ 
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
-
+ 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -17,32 +19,41 @@ import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+ 
 public class MainActivity extends AppCompatActivity {
-
-
+ 
+ 
     private static final String CHANNEL_ID = "My_channel_id";
     private Button haha;
-
+    private Button dwa;
+    private Button fourth;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         createNotificationChannel();
-
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.banana2);
         haha = findViewById(R.id.bobbutton);
         haha.setOnClickListener( v -> {
             sendNotification();
         });
-
-
+        dwa = findViewById(R.id.pszycisknumerodos);
+        dwa.setOnClickListener( v -> {
+            sendNotificationLong();
+        });
+        fourth = findViewById(R.id.bananos);
+        fourth.setOnClickListener( v -> {
+            no1.sendNotification("Tytuł", "Zawartość", CHANNEL_ID,R.drawable.banana2, this);
+        });
+ 
     }
-
+ 
     void createNotificationChannel(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            CharSequence name = "kanał powiadomień";
-            String description = "opis kanału powiadomień";
+            CharSequence name = "Kanał Powiadomień";
+            String description = "Opis Kanału Powiadomień";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
@@ -50,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             notifiactionManager.createNotificationChannel(channel);
         }
     }
-
+ 
     void sendNotification(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
             if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
@@ -59,22 +70,49 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         }
-
-
+ 
+ 
         Intent intent = new Intent(this, MainActivity.class);
-
+ 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-
+ 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.banan)
-                .setContentTitle("nowe powiadomienie")
-                .setContentText("tekst powiadomeinia")
+                .setSmallIcon(R.drawable.banana2)
+                .setContentTitle("Nowe powiadomienie")
+                .setContentText("Tekst Powiadomeinia")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(1, builder.build());
     }
-
-
+ 
+    void sendNotificationLong(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+ 
+ 
+        Intent intent = new Intent(this, MainActivity.class);
+ 
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+ 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.banana2)
+                .setContentTitle("Nowe powiadomienie")
+                .setContentText("Tekst Powiadomeinia")
+//
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+    }
+ 
+ 
 }
